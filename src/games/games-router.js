bf217dd937e6ec1c032a47ac093f60b1c3dbbe44
Charlie Kim <cwkim3@gmail.com>
2020-01-27 16:58:13 -0500

    implemented /api/games/attendance endpoints

--- src/games/games-router.js
+++ src/games/games-router.js
@@ -8,8 +8,6 @@ const {requireAuth} = require('../middleware/basic-auth')
 gamesRouter
     .route('/api/games')
     .post(requireAuth, jsonBodyParser, (req, res, next)=> {
-        console.log('POST GAME /api/games - req.user', req.user)
-
         const { game_name, game_date, game_time, game_street, game_city, game_state, game_zip, game_lat, game_lng } = req.body
         const newGame = { game_name, game_date, game_time, game_street, game_city, game_state, game_zip, game_lat, game_lng }
         
@@ -24,7 +22,6 @@ gamesRouter
     .get((req, res, next)=> {
         GamesService.getGames(req.app.get('db'))
             .then(games => {
-                console.log(games)
                 res.json(games)
 
             })
@@ -44,33 +41,79 @@ gamesRouter
     })
 
 
-    gamesRouter
+gamesRouter
     .route('/api/games/attendance')
-    .post(requireAuth, jsonBodyParser, (req, res, next)=> {
+    .post(requireAuth, jsonBodyParser, (req, res, next)=> { 
         const {game_id} = req.body
         const attending_user = req.user.id
 
-        const rsvpObj = {
-            game_id,
-            attending_user
-        }
+        const rsvpObj = { game_id, attending_user }
 
-        console.log('rsvpObj', rsvpObj)
-
-        GamesService.incrementGameRsvp(req.app.get('db'), rsvpObj)
-            .then(rsvp => {
-                console.log('RSVP:', rsvp)
-                GamesService.gameAttendanceCounter(req.app.get('db'), rsvpObj.game_id)
-                    .then(rsvpCount => {
-                        console.log('RSVP COUNT', rsvpCount.count)
-                        res.send(rsvpCount.count)
-                    })
-                
+        // check to see if attending_user already exists in game
+        GamesService.isUserAlreadyAttending(req.app.get('db'), rsvpObj.game_id, rsvpObj.attending_user)
+            .then(userIsAlreadyAttending => {
+                if(userIsAlreadyAttending){
+                    return 
+                } else {
+                    // if user is not already attending, increment game, get count
+                    GamesService.incrementGameRsvp(req.app.get('db'), rsvpObj)
+                        .then(rsvp => {
+                            GamesService.gameAttendanceCounter(req.app.get('db'), rsvpObj.game_id)
+                                .then(rsvpCount => {
+                                    res.send(rsvpCount.count)
+                                })                            
+                        })
+                }
             })
             .catch(next)
-        
-
     })
+
+
+gamesRouter
+    .route('/api/games/attendance/:game_id')
+    .get(requireAuth, (req, res, next)=> {
+        const game_id = req.params.game_id
+        const user_id = req.user.id
+
+        GamesService.getGameAttendance(req.app.get('db'), game_id)
+            .then(attendance => {
+                let isUserAttending = attendance.filter(game => game.attending_user === user_id)[0]
+                let userAttendance;
+
+                if (isUserAttending){
+                    userAttendance = true 
+                } else {
+                    userAttendance = false
+                }
+
+                res.json(userAttendance)
+            })        
+    })
+    .delete(requireAuth, (req, res, next)=> {
+        const attending_user = req.user.id
+        const game_id = req.params.game_id
+
+        GamesService.deleteGameAttendance(req.app.get('db'), game_id, attending_user)
+            .then(game => {
+                GamesService.gameAttendanceCounter(req.app.get('db'), game_id)
+                .then(rsvpCount => {
+                    res.send(rsvpCount.count)
+                })                         
+            })
+            .catch(next)
+    })
+
+gamesRouter
+    .route('/api/games/attendance/rsvp/:game_id')
+    .get(requireAuth, (req, res, next)=> {
+        const game_id = req.params.game_id
+        GamesService.gameAttendanceCounter(req.app.get('db'), game_id)
+            .then(rsvpCount => {
+                res.send(rsvpCount.count)
+            })
+            .catch(next)
+    })
+
 
 module.exports = gamesRouter
     
--- src/games/games-service.js
+++ src/games/games-service.js
@@ -24,12 +24,40 @@ const GamesService = {
             .returning('*')
     },
 
-    gameAttendanceCounter(db, gameId){
+    deleteGameAttendance(db, game_id, attending_user){
+        return db('postup_games_attendance')
+            .where({game_id})
+            .where({attending_user})
+            .del()
+    },
+
+    gameAttendanceCounter(db, game_id){
         return db('postup_games_attendance')
             .count('*')
-            .where({game_id: gameId})
+            .where({game_id})
             .returning('*')
             .first()
+    },
+
+    getGameAttendance(db, game_id){
+        return db('postup_games_attendance')
+            .where({game_id})
+            .returning('*')
+    },
+
+    isUserAlreadyAttending(db, game_id, attending_user){
+        return db('postup_games_attendance')
+            .select('*')
+            .where({game_id})
+            .then(attendance => {
+                let userAttending = attendance.filter(game => game.attending_user === attending_user)
+                if (userAttending.length > 0){
+                    return true
+                } else {
+                    return false
+                }
+                
+            })
     }
     
 
